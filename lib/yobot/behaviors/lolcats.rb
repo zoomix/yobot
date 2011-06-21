c1122f52f0e96acd76ffad36af7abf0ba399b39a
plukevdh <hungerandthirst@gmail.com>
2011-06-21 15:22:42 -0400

    fix lolcats (much like xkcd)

--- lib/yobot/behaviors/lolcats.rb
+++ lib/yobot/behaviors/lolcats.rb
@@ -1,19 +1,19 @@
-class Yobot::Behaviors::LolCats
+class Yobot::Behaviors::Lolcats
   BASE_URL = 'http://icanhascheezburger.com/?random#top'
   COMMAND = /^lolcats/
 
   def react(room, message)
     if message =~ COMMAND
-      room.text(get_lolcats) {}
+      get_lolcats(room)
     end
-    @log = Logging.logger["CampfireBot::Plugin::Lolcat"]
   end
   
-  def get_lolcats
+  def get_lolcats(room)
     # Scrape random lolcat
-    http = EventMachine::HttpRequest.new(BASE_URL)
+    http = EventMachine::HttpRequest.new(BASE_URL).get :redirects => 1
     http.callback do
-      Nokogiri::HTML.parse(http.response).css('div.entry img').first['src']
+      imgEl = Nokogiri::HTML.parse(http.response).css('div.entry img').first
+      room.text(imgEl['src']) {} if imgEl
     end
   end
 end
